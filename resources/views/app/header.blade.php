3551c0e8b3532356e4788f02498716de2f668bab
ThangBui <thangbui1296@gmail.com>
2019-11-18 22:38:46 +0700

    create header

--- resources/views/app/header.blade.php
+++ resources/views/app/header.blade.php
@@ -1,19 +1,9 @@
 <div class="col-md-12 container-flex">
-	header
-<!--     <nav class="navbar navbar-default" role="navigation">
+    <nav class="navbar navbar-default" role="navigation">
 		<div class="container-fluid">
-			Brand and toggle get grouped for better mobile display
 			<div class="navbar-header">
-				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
-					<span class="sr-only">Toggle navigation</span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-				</button>
-				<a class="navbar-brand" href="#">Title</a>
+				<a class="navbar-brand" href="#">X-Film</a>
 			</div>
-	
-			Collect the nav links, forms, and other content for toggling
 			<div class="collapse navbar-collapse navbar-ex1-collapse">
 
 				<form class="navbar-form navbar-left" role="search">
@@ -25,6 +15,6 @@
 				
 			</div>
 		</div>
-	</nav> -->
+	</nav>
 </div>
 
